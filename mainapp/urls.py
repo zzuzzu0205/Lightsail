e6bdad5b4c19e020a5dbd6b2292f21fead451d9b
gabang2 <offbeat1020@naver.com>
2022-03-07 23:39:26 +0900

    middle commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from mainapp.views import AccountCreateView, AccountDetailView
 
 app_name = 'mainapp'
 
 urlpatterns = [
-
     path('', TemplateView.as_view(template_name="mainapp/main_page.html"), name='main'),
+    path('signup/', AccountCreateView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='mainapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('account/<int:pk>', AccountDetailView.as_view(), name='account')
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,32 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm  # 기본적인 userform을 제공해준다.
-    success_url = reverse_lazy('mainapp:main')  # reverse는 함수형, reverse_lazy는 class에서 사욯한다.
+    success_url = reverse_lazy('mainapp:login')  # reverse는 함수형, reverse_lazy는 class에서 사욯한다.
     template_name = 'mainapp/signup.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'mainapp/account_detail.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
