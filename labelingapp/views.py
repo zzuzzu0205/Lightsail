46e1bee6b8868320446b65d24688ce376f2d51b9
dywudmin <mindu6424@naver.com>
2022-04-12 19:50:30 +0900

    20220412

--- labelingapp/views.py
+++ labelingapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, resolve
 
-from mainapp.models import Category, Review, FirstLabeledData
+from mainapp.models import Category, Review, FirstLabeledData, SecondLabeledData
 
 
 # start~end 범위 내 처리 안된 데이터 보여줌
@@ -14,6 +14,13 @@ def print_review(start, end, category_product):
                                               first_status=False, second_status=False, dummy_status=False).order_by(
         'review_number')[:1]
     return print_review_list
+
+def print_inspect(start, end, category_product):
+    print_review_inspect = Review.objects.filter(category_product=category_product,
+                                              review_number__range=(int(start), int(end)),
+                                              first_status=True, second_status=False, dummy_status=False).order_by(
+        'review_number')[:1]
+    return print_review_inspect
 
 
 def labeling_work(request):
@@ -52,9 +59,6 @@ def labeling_work(request):
                     # First_Labeled_Data모델을 불러와서 first_labeled_data에 저장
                     first_labeled_data = FirstLabeledData()
 
-                    # 하이라이트 기능
-
-
                     # laveling_work에서 불러온 값들을 first_labeled_data 안에 정해진 db이름으로 넣음
                     first_labeled_data.first_labeled_emotion = emotion  # 긍정 ,부정, 중립 저장
                     first_labeled_data.first_labeled_target = target  # 대상 저장
@@ -62,7 +66,6 @@ def labeling_work(request):
                     first_labeled_data.review_id = Review.objects.get(pk=review_id)
                     first_labeled_data.category_id = Category.objects.get(pk=category_id)
                     first_labeled_data.save()
-
 
                 # Next 버튼을 눌렀을 때
                 if request.method == "GET" and request.GET.get("form-type") == 'NextForm':
@@ -92,6 +95,72 @@ def labeling_work(request):
 
 
 def labeling_inspect(request):
+
+    try:
+
+        # reqeust한 URL의 파라미터에 제품군, 시작위치, 끝 위치가 있으면 데이터를 반환함
+        if 'category_product' in request.GET and 'start' in request.GET and 'end' in request.GET:
+
+            # 청소기, 냉장고, 식기세척기 제품군 선택 시에만 수행
+            if request.GET['category_product'] in ['cleaner', 'refrigerator', 'dish_washer']:
+                category_product = request.GET['category_product']
+                start = request.GET['start']
+                end = request.GET['end']
+
+                # 해당 제품군의 카테고리 정보 불러옴
+                category_detail = Category.objects.filter(category_product=category_product)
+
+                # 해당 제품군과 범위 중 제일 처음 한 개만 가져옴 => print_inspect() 함수 사용
+                review_first = print_inspect(start, end, category_product)
+
+                # labeling_inspect.html에 보낼 context 데이터
+                context = {'category_detail': category_detail, 'category_product': category_product,
+                           'review_first': review_first, 'start': start, 'end': end}
+
+                # POST 방식 request 받았을 때 수행함.
+                if request.method == "POST" and 'labeled_expression' in request.POST and 'labeled_target' in request.POST:
+
+                    # 들어온 값 변수에 저장
+                    target = request.POST.get('labeled_target')
+                    emotion = request.POST.get('labeled_emotion')
+                    expression = request.POST.get('labeled_expression')
+                    review_id = request.POST.get('review_id')  # 해당 리뷰 id 받아오기
+                    category_id = request.POST.get('category_id')  # 해당하는 리뷰에 맞는 카테고리id를 받아오기
+                    print(target, emotion, expression)
+
+                    # SecondLabeledData모델을 불러와서 second_labeled_data에 저장
+                    second_labeled_data = SecondLabeledData()
+
+                    # laveling_inspect에서 불러온 값들을 second_labeled_data 안에 정해진 db이름으로 넣음
+                    second_labeled_data.second_labeled_emotion = emotion  # 긍정 ,부정, 중립 저장
+                    second_labeled_data.second_labeled_target = target  # 대상 저장
+                    second_labeled_data.second_labeled_expression = expression  # 현상 저장
+                    second_labeled_data.review_id = Review.objects.get(pk=review_id)
+                    second_labeled_data.category_id = Category.objects.get(pk=category_id)
+                    second_labeled_data.save()
+
+                # Next 버튼을 눌렀을 때
+                if request.method == "GET" and request.GET.get("form-type") == 'NextForm':
+                    review_id = request.GET.get('review_id')
+
+                    # 해당 review의 작업 상태와 작업자를 변경
+                    Review.objects.filter(pk=review_id).update(second_status=True, labeled_user_id=request.user)
+
+                elif request.GET.get("form-type") == 'DummyForm':
+                    review_id = request.GET.get('review_id')
+                    Review.objects.filter(pk=review_id).update(second_status=False, dummy_status=True,
+                                                               labeled_user_id=request.user)
+
+                return render(request, 'labelingapp/labeling_inspect.html', context)
+
+            return render(request, 'labelingapp/labeling_inspect.html')
+
+        else:
+            context = {'message': '제품, 범위를 다시 선택해주세요.'}
+            return render(request, 'labelingapp/labeling_inspect.html', context)
+
+    # 예외처리
+    except Exception as identifier:
+        print(identifier)
+
     return render(request, 'labelingapp/labeling_inspect.html')
-
-
