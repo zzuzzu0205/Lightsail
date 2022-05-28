ed628ec29de34edf90b6402c1be7cf0dd721d71c
dywudmin <mindu6424@naver.com>
2022-05-29 05:51:05 +0900

    prod.py 수정

--- LG_Project/settings/prod.py
+++ LG_Project/settings/prod.py
@@ -1,6 +1,22 @@
 from .base import *
 
+import environ
+
 ALLOWED_HOSTS = ['43.200.38.56']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
